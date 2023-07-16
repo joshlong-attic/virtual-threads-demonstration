29c23948785aa167da867b1d44b5aed8ffabd1ab
Cora Iberkleid <ciberkleid@gmail.com>
2023-07-16 09:23:04 -0400

    Split into 2 demo options: Tomcat thread constraints (working) vs Docker resource constraints (WIP)

---
+++ demo-platformthreads/src/test/java/com/example/demo/TestContainerConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
+
+public interface TestContainerConfig {
+
+    GenericContainer<?> httpbin = new GenericContainer<>("mccutchen/go-httpbin")
+            .withExposedPorts(8080)
+            .waitingFor(new HttpWaitStrategy().forPort(8080))
+            .withReuse(false);
+
+    @DynamicPropertySource
+    static void containerProperties(DynamicPropertyRegistry registry) {
+        System.setProperty("httpbinUrl", "http://" + httpbin.getHost() + ":" + httpbin.getFirstMappedPort().toString());
+    }
+
+}
